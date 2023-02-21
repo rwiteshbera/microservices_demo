25dd5721181361269ee893f6376f33c2797b46aa
rwiteshbera <rwiteshbera@gmail.com>
2023-02-22 00:34:39 +0530

    fixed auth

--- authenticationService/database/users.go
+++ authenticationService/database/users.go
@@ -1,27 +1,8 @@
 package database
 
 import (
-	"database/sql"
-	"time"
-
 	"github.com/google/uuid"
 )
-
-const dbTimeOut = time.Second * 3
-
-var db *sql.DB
-
-func New(dbPool *sql.DB) Models {
-	db = dbPool
-
-	return Models{
-		user: User{},
-	}
-}
-
-type Models struct {
-	user User
-}
 
 type User struct {
 	Id        uuid.UUID `json:"id"`
